f2c85d2bf11e6afebca79539c65c64a124160ae5
KyleLoo0520 <loojq-wm23@student.tarc.edu.my>
2025-08-19 15:50:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.c9qhpxf7jkpg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "loojieqi-employee"
 customregion = "us-east-1"
-
